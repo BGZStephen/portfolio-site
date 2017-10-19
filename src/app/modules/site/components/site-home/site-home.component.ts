ac695134f0c37c84fe0628be7e0cd34a85406a9a
Stephen Wright <sjw948@gmail.com>
2017-10-19 20:32:54 +0100

    global styles setup + styles for site-home full-width

--- src/app/modules/site/components/site-home/site-home.component.ts
+++ src/app/modules/site/components/site-home/site-home.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-site-home',
   templateUrl: './site-home.component.html',
-  styleUrls: ['./site-home.component.scss']
 })
 export class SiteHomeComponent implements OnInit {
 
