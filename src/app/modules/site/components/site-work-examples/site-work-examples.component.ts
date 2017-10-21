98bd55b26cd812bc4fe809df66a0d863ed998693
Stephen Wright <sjw948@gmail.com>
2017-10-21 11:06:47 +0100

    ng template for workExamples

--- src/app/modules/site/components/site-work-examples/site-work-examples.component.ts
+++ src/app/modules/site/components/site-work-examples/site-work-examples.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'app/services/api.service'
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-site-work-examples',
@@ -8,9 +11,21 @@ export class SiteWorkExamplesComponent implements OnInit {
 
   exampleType: String = 'front-end-js'
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private apiService: ApiService,
+  ) {
+    const params = activatedRoute.params.map(param => param.type)
+    .subscribe(type => {
+      this.exampleType = type;
+    })
+  }
 
   ngOnInit() {
+    this.apiService.workExamples.getAll()
+    .subscribe(res => {
+      console.log(res)
+    })
   }
 
 }
