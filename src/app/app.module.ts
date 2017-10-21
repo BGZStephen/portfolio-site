5dc0ccdf8e4b97d15db366b1ec18032af811eae2
Stephen Wright <sjw948@gmail.com>
2017-10-21 11:05:48 +0100

    apiService improvements + added as a provided service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 // modules
 import { SiteModule } from 'app/modules/site/site.module';
@@ -10,16 +11,20 @@ import { AppComponent } from './app.component';
 // routes
 import { AppRoutes } from 'app/app.routes';
 
+// services
+import { ApiService } from 'app/services/api.service';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     AppRoutes,
     SiteModule,
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
