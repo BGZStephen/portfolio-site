5108c8bf03ccf845ec2d85e97fa0423456d2ff1d
Stephen Wright <sjw948@gmail.com>
2017-10-24 21:13:14 +0100

    primary menu visibility js toggle

--- src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -8,6 +8,7 @@ import { MenuBuilder } from 'app/services/menu-builder.service';
 export class DashboardNavbarComponent implements OnInit {
 
   navbarMenu: Array<object>;
+  menuVisible = false;
 
   constructor(
     private menuBuilder: MenuBuilder
@@ -15,6 +16,34 @@ export class DashboardNavbarComponent implements OnInit {
 
   ngOnInit() {
     this.navbarMenu = this.menuBuilder.buildNavbarMenu();
+  }
+
+  toggleMenuVisibility() {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  primaryMenuStyle() {
+    if (!this.menuVisible && screen.width > 1024) {
+      this.menuVisible = true;
+    }
+
+    if (!this.menuVisible) {
+      return {height: 0};
+    }
+
+    if (this.menuVisible && screen.width > 1024) {
+      return {height: 'auto'};
+    }
+
+    if (this.menuVisible && screen.width < 1024) {
+      const menuLength = document.getElementsByClassName('menu-primary')[0].children.length
+      const menuSize = `${menuLength * 45}px`
+      return {height: `${menuSize}`};
+    }
+  }
+
+  secondaryMenuStyle() {
+    return;
   }
 
 }
