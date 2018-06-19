9e4a410f351614f17872d7ecd31137b5d15fcd24
stephen <sjw948@gmail.com>
2018-06-19 20:16:15 +0100

    oznejs impot

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'core-js/es7/reflect';
+import 'zone.js';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
